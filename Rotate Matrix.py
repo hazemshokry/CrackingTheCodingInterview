71cfa0de9c0d458db5343e42238e27b8756336a5
hazem_sayed <hazem_sayed@apple.com>
2019-11-21 15:06:11 -0600

    1.7 Solve Matrix 90 degree rotation. Add Geekforgeeks solution. Edit in place

--- Rotate Matrix.py
+++ Rotate Matrix.py
@@ -20,6 +20,36 @@ def rotateMatrix(mat, n):
 
     return newmat
 
+# Geeksforgeek solution
+def rotateMatrix2(mat, n):
+    """
+    :param mat:
+    :return:
+    """
+    # Consider all squares one by one
+    for row in range(0, int(n / 2)):
+
+        # Consider elements in group
+        # of 4 in current square
+        for col in range(row, n - row - 1):
+            # store current cell in temp variable
+            temp = mat[row][col]
+
+            # move values from right to top
+            mat[row][col] = mat[col][n - 1 - row]
+
+            # move values from bottom to right
+            mat[col][n - 1 - row] = mat[n - 1 - row][n - 1 - col]
+
+            # move values from left to bottom
+            mat[n - 1 - row][n - 1 - col] = mat[n - 1 - col][row]
+
+            # assign temp to left
+            mat[n - 1 - col][row] = temp
+
+
+    return mat
+
 
 if __name__ == '__main__':
     mat = [[1, 2, 3, 4],
