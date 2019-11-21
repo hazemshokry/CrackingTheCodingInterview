225af6fbf29b93ab555eee30a8d0dba38a5da2ae
hazem_sayed <hazem_sayed@apple.com>
2019-11-20 23:01:10 -0600

    1.4 Solve Palindrome Permutation problem. Using reduce and filter

--- Palindrome Permutation.py
+++ Palindrome Permutation.py
@@ -2,6 +2,8 @@
 # A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
 # The palindrome does not need to be limited to just dictionary words.
 # Example - Input: Tact Coa -- Output: True ("taco cat, atco cta, etc...)
+from functools import reduce
+
 
 def palindromePermutation(string):
     """
@@ -10,13 +12,11 @@ def palindromePermutation(string):
     """
     string = string.lower()
     dict = {char: string.count(char) for char in string if char != " "}
-    oddCount = 0
-    for i in dict:
-        if dict[i] % 2 == 1:
-            oddCount += 1
-    if oddCount % 2 == 0:
-        return False
-    return True
+    oddCount = reduce((lambda a,b: a+b),filter(lambda x: x if x%2 == 1 else 0, dict.values()))
+
+    if oddCount == 1:
+        return True
+    return False
 
 if __name__ == '__main__':
-    print(palindromePermutation("carerac"))
+    print(palindromePermutation("Tact Coa"))
